d76085d9a9d8b743bf4a58a29baa6e1981cc9697
Noah <njy_roxy@outlook.com>
2023-03-07 11:28:11 +0800

    :sparkles: 多线程gcc编译要带上 -lpthread, 告诉连接器链接上pthread库

--- ch5/thread.c
+++ ch5/thread.c
@@ -14,32 +14,41 @@ int main(int argc, char** argv)
 	pthread_t	tid;
 	pthread_attr_t	thread_attr;
 
+	// 初始化结构体
 	if( pthread_attr_init(&thread_attr) )
 	{
 		printf("pthread_attr_init() failure：%s\n", strerror(errno));
 		return -1;
 	}
 
+	// 设置栈区大小为120KB
 	if( (pthread_attr_setstacksize(&thread_attr, 120*1024)) < 0 )
 	{
 		printf("pthread_attr_setstacksize() failure: %s\n", strerror(errno));
 		return -2;
 	}
 
+	// 设置线程属性->分离状态为分离
 	if( pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_DETACHED) )
 	{
 		printf("pthread_attr_setdetachstate() failure: %s\n", strerror(errno));
 		return -3;
 	}
 
+	// 创建线程，返回线程的线程IP给tid， 线程属性为thread_attr，
+	// 线程调用的函数为thread_worker1，
+	// shared_var是需要传给线程的参数，如果有多个参数需要传给子线程函数，
+	// 则需要将多个参数封装到一个结构体里再传进去
 	pthread_create(&tid ,&thread_attr, thread_worker1, &shared_var);
 	printf("Thread worker1 tid[%ld] created ok\n", tid);
 
+	// 使用默认结构体，故thread_worker2的分离属性是默认的是可会合
 	pthread_create(&tid, NULL, thread_worker2, &shared_var);
 	printf("Thread worker2 tid[%ld] created ok\n", tid);
 
-	// Wait until thread worker2 exit()
-	// 等待直到worker2线程使用exit()退出
+	// 创建子线程完毕，线程属性结构体使用完毕，摧毁释放
+	pthread_attr_destroy(&thread_attr);
+	// 主线程阻塞，等待直到可会合线程执行完毕（既thread_worker2)
 	pthread_join(tid, NULL);
 
 	while(1)
